458c93925267da5ee63b5a61daa488c2ea37cdca
chacha912 <dkwkdnwk912@gmail.com>
2021-11-29 23:18:33 +0900

    refactor: Move habit logic from app to habit_presenter

--- src/app.jsx
+++ src/app.jsx
@@ -8,39 +8,30 @@ import Navbar from './components/navbar';
 const App = ({ presenter }) => {
   const [habits, setHabits] = useState(presenter.getHabits());
 
-  const handleIncrement = useCallback((habit) => {
-    presenter.increment(habit, setHabits);
-  }, []);
+  const handleIncrement = useCallback(
+    (habit) => {
+      presenter.increment(habit, setHabits);
+    },
+    [presenter]
+  );
 
-  const handleDecrement = useCallback((habit) => {
-    setHabits((habits) =>
-      habits.map((item) => {
-        if (item.id === habit.id) {
-          const count = habit.count - 1;
-          return { ...habit, count: count < 0 ? 0 : count };
-        }
-        return item;
-      })
-    );
-  }, []);
+  const handleDecrement = useCallback(
+    (habit) => {
+      presenter.decrement(habit, setHabits);
+    },
+    [presenter]
+  );
 
   const handleDelete = useCallback((habit) => {
-    setHabits((habits) => habits.filter((item) => item.id !== habit.id));
+    presenter.delete(habit, setHabits);
   }, []);
 
   const handleAdd = useCallback((name) => {
-    setHabits((habits) => [...habits, { id: Date.now(), name, count: 0 }]);
+    presenter.add(name, setHabits);
   }, []);
 
   const handleReset = useCallback(() => {
-    setHabits((habits) =>
-      habits.map((habit) => {
-        if (habit.count !== 0) {
-          return { ...habit, count: 0 };
-        }
-        return habit;
-      })
-    );
+    presenter.reset(setHabits);
   }, []);
 
   return (
