5a8019f8eed92f3f501fb70aac271ce8e00c751d
chacha912 <dkwkdnwk912@gmail.com>
2021-11-30 00:01:52 +0900

    style: fix typo

--- src/test/habit_presenter.test.js
+++ src/test/habit_presenter.test.js
@@ -5,82 +5,82 @@ describe('HabitPresenter', () => {
     { id: 1, name: 'first-habit', count: 1 },
     { id: 2, name: 'second-habit', count: 0 },
   ];
-  let habitPresenter;
-  let callBackFn;
+  let presenter;
+  let update;
 
   beforeEach(() => {
-    callBackFn = jest.fn();
-    habitPresenter = new HabitPresenter(habits);
+    update = jest.fn();
+    presenter = new HabitPresenter(habits);
   });
 
   it('inits with habits', () => {
-    expect(habitPresenter.getHabits()).toEqual(habits);
+    expect(presenter.getHabits()).toEqual(habits);
   });
 
   it('increments habit count and call update callback', () => {
-    habitPresenter.increment(habits[0], callBackFn);
+    presenter.increment(habits[0], update);
 
-    expect(habitPresenter.getHabits()[0].count).toBe(2);
-    expect(callBackFn).toHaveBeenCalledTimes(1);
-    expect(callBackFn).toHaveBeenCalledWith(habitPresenter.getHabits());
+    expect(presenter.getHabits()[0].count).toBe(2);
+    expect(update).toHaveBeenCalledTimes(1);
+    expect(update).toHaveBeenCalledWith(presenter.getHabits());
   });
 
   describe('decrement', () => {
     it('regular', () => {
-      habitPresenter.decrement(habits[0], callBackFn);
+      presenter.decrement(habits[0], update);
 
-      expect(habitPresenter.getHabits()).toEqual(
+      expect(presenter.getHabits()).toEqual(
         habits.map((habit) => {
           if (habit.id === 1) return { ...habit, count: habit.count - 1 };
           return habit;
         })
       );
 
-      expect(callBackFn).toHaveBeenCalledTimes(1);
+      expect(update).toHaveBeenCalledTimes(1);
     });
 
     it('minus', () => {
-      habitPresenter.decrement(habits[1], callBackFn);
+      presenter.decrement(habits[1], update);
 
-      expect(habitPresenter.getHabits()).toEqual(
+      expect(presenter.getHabits()).toEqual(
         habits.map((habit) => {
           if (habit.id === 2) return { ...habit, count: 0 };
           return habit;
         })
       );
 
-      expect(callBackFn).toHaveBeenCalledTimes(1);
+      expect(update).toHaveBeenCalledTimes(1);
     });
   });
 
   it('delete', () => {
-    habitPresenter.delete(habits[0], callBackFn);
+    presenter.delete(habits[0], update);
 
-    expect(habitPresenter.getHabits()).toEqual(
+    expect(presenter.getHabits()).toEqual(
       habits.filter((habit) => habit.id !== 1)
     );
 
-    expect(callBackFn).toHaveBeenCalledTimes(1);
+    expect(update).toHaveBeenCalledTimes(1);
   });
 
   it('add', () => {
-    habitPresenter.add('new-habit', callBackFn);
+    presenter.add('new-habit', update);
 
-    expect(habitPresenter.getHabits()).toEqual([
+    expect(presenter.getHabits()).toEqual([
       ...habits,
       { id: Date.now(), name: 'new-habit', count: 0 },
     ]);
 
-    expect(callBackFn).toHaveBeenCalledTimes(1);
+    expect(update).toHaveBeenCalledTimes(1);
   });
 
   it('reset', () => {
-    habitPresenter.reset(callBackFn);
+    presenter.reset(update);
 
-    expect(habitPresenter.getHabits()).toEqual(
+    expect(presenter.getHabits()).toEqual(
       habits.map((habit) => ({ ...habit, count: 0 }))
     );
 
-    expect(callBackFn).toHaveBeenCalledTimes(1);
+    expect(update).toHaveBeenCalledTimes(1);
   });
 });
