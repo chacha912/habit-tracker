69968ad6aecf07eded4933a322768fb4091fa295
chacha912 <dkwkdnwk912@gmail.com>
2021-11-30 16:54:12 +0900

    test: component > habitAddForm - snspshot test

--- src/components/form/tests/habitAddForm.test.js
+++ src/components/form/tests/habitAddForm.test.js
@@ -2,31 +2,40 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import HabitAddForm from '../habitAddForm';
+import renderer from 'react-test-renderer';
 
 describe('HabitAddForm', () => {
   let onAdd;
   let input;
   let button;
 
-  beforeEach(() => {
-    onAdd = jest.fn();
-    // given
-    render(<HabitAddForm onAdd={onAdd} />);
-    input = screen.getByPlaceholderText('Habit');
-    button = screen.getByText('Add');
+  it('renders', () => {
+    // 스냅샷 테스트
+    const component = renderer.create(<HabitAddForm onAdd={jest.fn()} />);
+    expect(component.toJSON()).toMatchSnapshot();
   });
 
-  it('calls onAdd when button is clicked and valid habit is entered', () => {
-    userEvent.type(input, 'New Habit');
-    userEvent.click(button);
+  describe('Form Submit', () => {
+    beforeEach(() => {
+      onAdd = jest.fn();
+      // given
+      render(<HabitAddForm onAdd={onAdd} />);
+      input = screen.getByPlaceholderText('Habit');
+      button = screen.getByText('Add');
+    });
 
-    expect(onAdd).toHaveBeenCalledWith('New Habit');
-  });
+    it('calls onAdd when button is clicked and valid habit is entered', () => {
+      userEvent.type(input, 'New Habit');
+      userEvent.click(button);
 
-  it('does not call onAdd when the habit is empty', () => {
-    userEvent.type(input, '');
-    userEvent.click(button);
+      expect(onAdd).toHaveBeenCalledWith('New Habit');
+    });
 
-    expect(onAdd).toBeCalledTimes(0);
+    it('does not call onAdd when the habit is empty', () => {
+      userEvent.type(input, '');
+      userEvent.click(button);
+
+      expect(onAdd).toBeCalledTimes(0);
+    });
   });
 });
