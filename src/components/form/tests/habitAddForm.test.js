0299b256b30e60e34c16dd4365d8a4b39c7d391a
chacha912 <dkwkdnwk912@gmail.com>
2021-11-30 16:09:03 +0900

    test: component > habitAddForm - empty input check

--- src/components/form/tests/habitAddForm.test.js
+++ src/components/form/tests/habitAddForm.test.js
@@ -5,27 +5,28 @@ import HabitAddForm from '../habitAddForm';
 
 describe('HabitAddForm', () => {
   let onAdd;
+  let input;
+  let button;
 
   beforeEach(() => {
     onAdd = jest.fn();
-  });
-
-  it('calls onAdd when button is clicked', () => {
     // given
     render(<HabitAddForm onAdd={onAdd} />);
-    const input = screen.getByPlaceholderText('Habit');
-    const button = screen.getByText('Add');
+    input = screen.getByPlaceholderText('Habit');
+    button = screen.getByText('Add');
+  });
 
-    // when
-    // input 에 원하는 습관의 이름을 타이핑한 다음에
-    // add 라는 버튼을 클릭하면
+  it('calls onAdd when button is clicked and valid habit is entered', () => {
     userEvent.type(input, 'New Habit');
     userEvent.click(button);
-    // fireEvent 하위레벨의 이벤트를 발생 -> 버튼 focus 안됨
-    // userEvent 실제 사용자가 클릭하는것처럼 효과
 
-    // then
-    // onAdd 가 input에 입력된 이름과 함께 호출되어야 함!
     expect(onAdd).toHaveBeenCalledWith('New Habit');
+  });
+
+  it('does not call onAdd when the habit is empty', () => {
+    userEvent.type(input, '');
+    userEvent.click(button);
+
+    expect(onAdd).toBeCalledTimes(0);
   });
 });
