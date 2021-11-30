23697a27403f1ab41a1780574f48c70b648d8e3f
chacha912 <dkwkdnwk912@gmail.com>
2021-11-30 22:25:11 +0900

    test: Habit Tracker > render test using findByText

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,6 +1,13 @@
-describe('app', () => {
+/// <reference types="cypress" />
+import '@testing-library/cypress/add-commands';
+
+describe('Habit Tracker', () => {
   beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('renders', () => {
+    cy.findByText('Habit Tracker').should('be.visible');
   });
 
   it('displays three habits by default', () => {
